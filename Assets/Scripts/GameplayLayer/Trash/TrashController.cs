a9edd5105f53b7299162aab8b5dacace163222b1
Harryanto Winsen <harry@hwinsen.com>
2022-10-20 14:31:28 +0700

    Fix double trash spawn

--- Assets/Scripts/GameplayLayer/Trash/TrashController.cs
+++ Assets/Scripts/GameplayLayer/Trash/TrashController.cs
@@ -12,7 +12,7 @@ namespace EcoTeam.EcoToss.Trash
     {
         private Quaternion _defaultRotation;
         private Rigidbody _rigidbody;
-        private bool _hasCollided;
+        private bool _hasCollided = false;
         private float _stayDuration = 0f;
         private float _stayMaxDuration = 1.5f;
 
@@ -39,32 +39,28 @@ namespace EcoTeam.EcoToss.Trash
                 Debug.Log(transform.name + "nabrak" + collision.transform.name);
             }
 
-            if (collision.gameObject.CompareTag("Ground") ||
-                collision.gameObject.CompareTag("Intruder") ||
-                collision.gameObject.tag.Substring(0, 8) == "TrashCan")
-            {
-                if (_hasCollided == false)
+            if ((collision.gameObject.CompareTag("Ground") ||
+                collision.gameObject.tag.Substring(0, 8) == "TrashCan") && _hasCollided == false)
+            {       
+                _hasCollided = true;
+                Debug.Log("has collided= " + _hasCollided);
+                if (collision.gameObject.CompareTag("Ground"))
                 {
-                    _hasCollided = true;
-
-                    if (collision.gameObject.CompareTag("Ground"))
-                    {
-                        PublishSubscribe.Instance.Publish<MessageDecreaseHealth>(new MessageDecreaseHealth());
-                        Invoke(nameof(StoreToPool), 0.5f);
-                    }
-                    else if (collision.gameObject.tag.Substring(0, 8) == "TrashCan" || collision.gameObject.CompareTag("Intruder"))
-                    {
-                        StoreToPool();
-                    }
-
-                    if (GameManagerController.Instance.IsWindSpawn)
-                    {
-                        PublishSubscribe.Instance.Publish<MessageSetRandomPropertiesWindArea>(new MessageSetRandomPropertiesWindArea());
-                    }
-
-                    //Debug.Log("spawn");
-                    PublishSubscribe.Instance.Publish<MessageTrashSpawn>(new MessageTrashSpawn());
+                    PublishSubscribe.Instance.Publish<MessageDecreaseHealth>(new MessageDecreaseHealth());
+                    Invoke(nameof(StoreToPool), 0.5f);
                 }
+                else if (collision.gameObject.tag.Substring(0, 8) == "TrashCan" || collision.gameObject.CompareTag("Intruder"))
+                {
+                    StoreToPool();
+                }
+
+                if (GameManagerController.Instance.IsWindSpawn)
+                {
+                    PublishSubscribe.Instance.Publish<MessageSetRandomPropertiesWindArea>(new MessageSetRandomPropertiesWindArea());
+                }
+
+                //Debug.Log("spawn");
+                PublishSubscribe.Instance.Publish<MessageTrashSpawn>(new MessageTrashSpawn());
             }
         }
 
@@ -79,6 +75,28 @@ namespace EcoTeam.EcoToss.Trash
                 StoreToPool();
                 PublishSubscribe.Instance.Publish<MessageTrashSpawn>(new MessageTrashSpawn());
                 _stayDuration = 0;
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log(transform.name + "nabrak" + other.transform.name);
+            }
+
+            if (other.gameObject.CompareTag("Intruder") && _hasCollided == false)
+            {       
+                _hasCollided = true;
+                Debug.Log("has collided= " + _hasCollided);
+                    StoreToPool();
+
+                if (GameManagerController.Instance.IsWindSpawn)
+                {
+                    PublishSubscribe.Instance.Publish<MessageSetRandomPropertiesWindArea>(new MessageSetRandomPropertiesWindArea());
+                }
+
+                //Debug.Log("spawn");
+                PublishSubscribe.Instance.Publish<MessageTrashSpawn>(new MessageTrashSpawn());
             }
         }
     }
