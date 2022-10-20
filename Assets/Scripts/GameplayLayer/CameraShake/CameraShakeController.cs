39fdbb3fe6951314f07d12243c99c6c2354bae78
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2022-10-20 13:21:31 +0700

    Add camera shake effect

---
+++ Assets/Scripts/GameplayLayer/CameraShake/CameraShakeController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EcoTeam.EcoToss.PubSub;
+using Agate.MVC.Core;
+
+namespace EcoTeam.EcoToss.CameraShake
+{
+    public class CameraShakeController : MonoBehaviour
+    {
+        private float duration = .5f;
+
+        private void Awake()
+        {
+            PublishSubscribe.Instance.Subscribe<MessageShakingCamera>(Shake);
+        }
+
+        private void OnDestroy()
+        {
+            PublishSubscribe.Instance.Unsubscribe<MessageShakingCamera>(Shake);
+        }
+
+        IEnumerator ShakeCoroutine()
+        {
+            Vector3 startPos = transform.position;
+            float elapsedTime = .1f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                transform.position = startPos + Random.insideUnitSphere;
+                yield return null;
+            }
+
+            transform.position = startPos;
+        }
+
+        public void Shake(MessageShakingCamera msg)
+        {
+            StartCoroutine(ShakeCoroutine());
+        }
+    }
+
+}
