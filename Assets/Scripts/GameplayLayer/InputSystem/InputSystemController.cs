18940bfd8b73d230c7c2192aa3a7afea05a130a7
Harryanto Winsen <harry@hwinsen.com>
2022-10-21 02:41:59 +0700

    Update InputSystemController.cs
    Change behavior to quick swipe style. Players now don't need to remove their finger after swiping.

--- Assets/Scripts/GameplayLayer/InputSystem/InputSystemController.cs
+++ Assets/Scripts/GameplayLayer/InputSystem/InputSystemController.cs
@@ -3,33 +3,61 @@ using EcoTeam.EcoToss.PubSub;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace EcoTeam.EcoToss.InputSystem
 {
     public class InputSystemController : MonoBehaviour
     {
         private Vector2 _touchStartPosition, _touchEndPosition, _swipeDirection;
+        private bool _fingerDown;
+        [SerializeField] private int _pixelToDetect = 50;
 
         void Update()
         {
-            // if you touch the screen
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            // Detect if there is touch input
+            if (_fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(0))
             {
-                _touchStartPosition = Input.GetTouch(0).position;
+                _touchStartPosition = Input.touches[0].position;
+                _fingerDown = true;
             }
 
-            // if you release your finger
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            if (_fingerDown && Time.timeScale == 1)
             {
-                // getting release finger position
-                _touchEndPosition = Input.GetTouch(0).position;
+                // Detect if player is swiping
+                if (Input.touches[0].position.x <= _touchStartPosition.x - _pixelToDetect ||
+                Input.touches[0].position.x >= _touchStartPosition.x + _pixelToDetect ||
+                Input.touches[0].position.y <= _touchStartPosition.y - _pixelToDetect ||
+                Input.touches[0].position.y >= _touchStartPosition.y + _pixelToDetect)
+                {
+                    _fingerDown = false;
+                    OnSwipe();
+                    if (Debug.isDebugBuild)
+                    {
+                        Debug.Log("Swipe");
+                    }
+                }
 
-                // calculating swipe direction in 2D space
-                _swipeDirection = _touchStartPosition - _touchEndPosition;
-                _swipeDirection = Vector3.Normalize(_swipeDirection);
-
-                PublishSubscribe.Instance.Publish<MessageTrashThrowing>(new MessageTrashThrowing(_swipeDirection));
+                // Detect if player is removing their touch input
+                if (Input.GetTouch(0).phase == TouchPhase.Ended ||
+                Input.GetTouch(0).phase == TouchPhase.Canceled ||
+                Time.timeScale == 0)
+                {
+                    _fingerDown = false;
+                }
             }
+        }
+
+        private void OnSwipe()
+        {
+            // getting release finger position
+            _touchEndPosition = Input.GetTouch(0).position;
+
+            // calculating swipe direction in 2D space
+            _swipeDirection = _touchStartPosition - _touchEndPosition;
+            _swipeDirection = Vector3.Normalize(_swipeDirection);
+            Debug.Log("Swipe direction: " + _swipeDirection);
+            PublishSubscribe.Instance.Publish<MessageTrashThrowing>(new MessageTrashThrowing(_swipeDirection));
         }
     }
 }
