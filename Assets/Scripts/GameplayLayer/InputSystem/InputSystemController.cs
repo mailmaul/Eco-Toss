3891ccc7cb56df7e505d361dda881c505d4a1781
Harryanto Winsen <harry@hwinsen.com>
2022-10-21 01:55:13 +0700

    Auto detect Trash to throw
    Remove Raycast from InputSystem

--- Assets/Scripts/GameplayLayer/InputSystem/InputSystemController.cs
+++ Assets/Scripts/GameplayLayer/InputSystem/InputSystemController.cs
@@ -8,24 +8,14 @@ namespace EcoTeam.EcoToss.InputSystem
 {
     public class InputSystemController : MonoBehaviour
     {
-        private Vector3 _touchRayPosition; // touch raycast
         private Vector2 _touchStartPosition, _touchEndPosition, _swipeDirection;
 
-        [SerializeField] Camera _mainCamera;
-        [SerializeField] Rigidbody _rigidbody;
-
-        // Update is called once per frame
         void Update()
         {
             // if you touch the screen
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                _touchRayPosition = Input.GetTouch(0).position;
-                _touchRayPosition.z = 100f;
-                _touchRayPosition = _mainCamera.ScreenToWorldPoint(_touchRayPosition);
-
                 _touchStartPosition = Input.GetTouch(0).position;
-                Raycast();
             }
 
             // if you release your finger
@@ -38,37 +28,7 @@ namespace EcoTeam.EcoToss.InputSystem
                 _swipeDirection = _touchStartPosition - _touchEndPosition;
                 _swipeDirection = Vector3.Normalize(_swipeDirection);
 
-                if (_rigidbody != null)
-                {
-                    PublishSubscribe.Instance.Publish<MessageTrashThrowing>(new MessageTrashThrowing(_rigidbody, _swipeDirection));
-
-                    // reset selected rigidbody
-                    _rigidbody = null;
-                }
-            }
-        }
-
-        private void Raycast()
-        {
-            RaycastHit hit;
-
-            Ray ray = new Ray(transform.position, _touchRayPosition - transform.position);
-            if (Debug.isDebugBuild)
-            {
-                Debug.DrawRay(transform.position, _touchRayPosition - transform.position, Color.red, 3f);
-            }
-
-            bool raycastIsHit = Physics.Raycast(ray, out hit);
-
-            if (raycastIsHit)
-            {
-                if (hit.collider != null && hit.collider.GetComponent<Rigidbody>() != null)
-                {
-                    if (hit.rigidbody.isKinematic)
-                    {
-                        _rigidbody = hit.rigidbody;
-                    }
-                }
+                PublishSubscribe.Instance.Publish<MessageTrashThrowing>(new MessageTrashThrowing(_swipeDirection));
             }
         }
     }
