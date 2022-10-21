3165e36599ef52b2f00c5264b29801c3b2f096fa
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2022-10-21 14:13:46 +0700

    Create UI random buff spin effect

--- Assets/Scripts/GameplayLayer/Buff/BaseBuff.cs
+++ Assets/Scripts/GameplayLayer/Buff/BaseBuff.cs
@@ -6,6 +6,12 @@ namespace EcoTeam.EcoToss.Buff
 {
     public abstract class BaseBuff : MonoBehaviour
     {
+        [SerializeField] protected Sprite _sprite;
         public abstract void BuffEffect();
+
+        public Sprite GetSprite()
+        {
+            return _sprite;
+        }
     }
 }
