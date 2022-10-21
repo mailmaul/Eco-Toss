e634e5d92460928e50119b9e9ee58abd7e8a4357
mailmaul <maulmailmaul@gmail.com>
2022-10-22 04:58:09 +0700

    Revert "Merge branch 'develop' into art/UI"
    This reverts commit fdd467294afc167d4c4d663fddc61371dad6ddde, reversing
    changes made to 5ae1c65d7e248a72fa365da702559f134eefcac1.

--- Assets/Scripts/GameplayLayer/Buff/BuffController.cs
+++ Assets/Scripts/GameplayLayer/Buff/BuffController.cs
@@ -3,13 +3,18 @@ using EcoTeam.EcoToss.PubSub;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace EcoTeam.EcoToss.Buff
 {
     public class BuffController : MonoBehaviour
     {
         [SerializeField] private List<BaseBuff> _buffList = new();
+        [SerializeField] private Image _currentImage;
+
         private int _randomIndex;
+        private float _currentTimerSpin;
+        [SerializeField] private float _timerSpin = 1f;
 
         private void Awake()
         {
@@ -21,16 +26,40 @@ namespace EcoTeam.EcoToss.Buff
             PublishSubscribe.Instance.Unsubscribe<MessagePlayBuff>(PlayBuff);
         }
 
+        private void Start()
+        {
+            _currentImage.sprite = _buffList[0].GetSprite();
+            _currentImage.gameObject.SetActive(false);
+            _currentTimerSpin = _timerSpin;
+        }
+
         private void PlayBuff(MessagePlayBuff message)
         {
-            _randomIndex = Random.Range(0, _buffList.Count);
+            StartCoroutine(RandomBuff());
+        }
 
+        IEnumerator RandomBuff()
+        {
+            _currentImage.gameObject.SetActive(true);
+            while (_currentTimerSpin >= 0)
+            {
+                _randomIndex = Random.Range(0, _buffList.Count);
+                _currentImage.sprite = _buffList[_randomIndex].GetSprite();
+                _currentTimerSpin -= Time.deltaTime * 10;
+                yield return new WaitForSeconds(.05f);
+            }
+
+            _currentTimerSpin = _timerSpin;
             _buffList[_randomIndex].BuffEffect();
 
             if (Debug.isDebugBuild)
             {
                 Debug.Log("get buff: " + _buffList[_randomIndex].name);
             }
+
+            yield return new WaitForSeconds(3);
+
+            _currentImage.gameObject.SetActive(false);
         }
     }
 }
